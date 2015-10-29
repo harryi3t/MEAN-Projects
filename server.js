8b11995837a092716e4a6ca32060ec25529d9680
harryi3t <66hari@gmail.com>
2015-10-29 10:54:46 +0530

    Removed hard coded port no.

--- server.js
+++ server.js
@@ -41,5 +41,7 @@ app.get('/*', function (req, res) {
 });
 
 // listen (start app with node server.js) ======================================
-app.listen(3000);
-console.log("App listening on port 3000");
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('listening on', port);
+});
